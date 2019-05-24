c29e277bef013556243bf972ea4beecbaf46cb4f
Tess <gard.tess@gmail.com>
2019-05-24 13:00:31 +1000

    finished undercooked timer functionality

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ playBtn.addEventListener("click", function (event) {
   document.querySelector(".main-game-container").style.display = "block";
 
   document.querySelector(".main-welcome-container").style.display = "none";
+
+  document.querySelector("body").classList.remove("body-background")
+
 });
 
 let boilBtn = document.querySelector(".boil-egg-btn");
@@ -14,35 +17,8 @@ boilBtn.addEventListener("click", function (event) {
   gameBox2.classList.add("game-box-2-on-click")
 
   gameBox2.classList.remove("game-box-2")
-  
 
   document.querySelector(".game-box").style.display = "none";
-});
-
-let removeEggBtn = document.querySelector(".boil-egg-btn-2");
-removeEggBtn.addEventListener("click", function (event) {
-
-  const gameBox3 = document.querySelector(".game-box-3");
-  gameBox3.classList.add("game-box-3-on-click")
-  gameBox3.classList.remove("game-box-3")
-
-  const gameBox2 = document.querySelector(".game-box-2-on-click");
-
-  gameBox2.style.display = "none";
-  
-});
-
-let eatBtn = document.querySelector(".finish-egg-btn");
-eatBtn.addEventListener("click", function (event) {
-
-  const gameBox4 = document.querySelector(".game-box-4");
-  gameBox4.classList.add("game-box-4-on-click")
-  gameBox4.classList.remove("game-box-4")
-
-  const gameBox3 = document.querySelector(".game-box-3-on-click");
-
-  gameBox3.style.display = "none";
-  
 });
 
 let againBtn = document.querySelector(".again-btn");
@@ -51,3 +27,4 @@ againBtn.addEventListener("click", function (event) {
   document.reload()
   
 });
+
--- counter.js
+++ counter.js
@@ -2,7 +2,6 @@
 boilBtn.addEventListener("click", function (event) {
 
 var timer = 0;
-console.log(timer)
 var eggTimer = setInterval(function(){
   document.getElementById("counter").innerHTML = timer;
   timer += 1;
@@ -13,7 +12,7 @@ var eggTimer = setInterval(function(){
 
     document.querySelector(".cook-instructions").style.display = "none"
 
-    document.querySelector(".boil-egg-btn-2").style.display = "none"
+    document.querySelector(".remove-egg-btn").style.display = "none"
 
     document.getElementById("egg-in-pan").style.display = "none"
 
@@ -23,5 +22,44 @@ var eggTimer = setInterval(function(){
   }
 
 }, 1000);
+
+let removeEggBtn = document.querySelector(".remove-egg-btn");
+removeEggBtn.addEventListener("click", function (event) {
+
+  if(timer >5 ){
+    const gameBox2 = document.querySelector(".game-box-2-on-click");
+    gameBox2.style.display = "none";
+
+    const gameBoxUnder = document.querySelector(".game-box-3");
+    gameBoxUnder.classList.add("game-box-3-on-click")
+    gameBoxUnder.classList.remove("game-box-3")
+    }
+
+  else if(timer <5 ){
+
+    const gameBox2 = document.querySelector(".game-box-2-on-click");
+    gameBox2.style.display = "none";
+
+    const gameBoxUnder = document.querySelector(".game-box-undercooked-display-none");
+    gameBoxUnder.classList.add("game-box-undercooked")
+    gameBoxUnder.classList.remove("game-box-undercooked-display-none")
+    }
+  
+});
+
+let eatBtn = document.querySelector(".eat-egg");
+eatBtn.addEventListener("click", function (event) {
+
+  const gameBox4 = document.querySelector(".game-box-4");
+  gameBox4.classList.add("game-box-4-on-click")
+  gameBox4.classList.remove("game-box-4")
+
+  const gameBox3 = document.querySelector(".game-box-3-on-click");
+
+  gameBox3.style.display = "none";
+  
+});
+
+
 
 });

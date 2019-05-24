595a1a59ab1d8d3d1ecd8a5a4cdcecd97840d360
Tess <gard.tess@gmail.com>
2019-05-24 11:26:16 +1000

    timer functionality for over-cooked

--- counter.js
+++ counter.js
@@ -1,9 +1,27 @@
-var timeleft = 10;
-var downloadTimer = setInterval(function(){
-  document.getElementById("counter").innerHTML = timeleft;
-  timeleft -= 1;
-  if(timeleft <= 0){
-    clearInterval(downloadTimer);
-    document.getElementById("counter").innerHTML = "egg is done!"
+
+boilBtn.addEventListener("click", function (event) {
+
+var timer = 0;
+console.log(timer)
+var eggTimer = setInterval(function(){
+  document.getElementById("counter").innerHTML = timer;
+  timer += 1;
+  if(timer == 10){
+    clearInterval(eggTimer);
+
+    document.getElementById("counter").innerHTML = "you have overcooked the egg :("
+
+    document.querySelector(".cook-instructions").style.display = "none"
+
+    document.querySelector(".boil-egg-btn-2").style.display = "none"
+
+    document.getElementById("egg-in-pan").style.display = "none"
+
+    const tryAgainBtn = document.querySelector(".try-again-display-non");
+    tryAgainBtn.classList.add("try-again-btn")
+    tryAgainBtn.classList.remove("try-again-display-non")
   }
+
 }, 1000);
+
+});
